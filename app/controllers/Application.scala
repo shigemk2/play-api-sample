8449c071f90b85d1c1878267878b9f89c50560d4
shigemk2 <i.am.shige@gmail.com>
2016-02-11 19:28:28 +0900

    Add apis 200 400 500

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,32 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.iteratee.Enumerator
 
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok("index")
   }
 
+  def ok = Action {
+    Result(
+      header = ResponseHeader(200, Map(CONTENT_TYPE -> "text/plain")),
+      body = Enumerator("OK!".getBytes())
+    )
+  }
+
+  def bad = Action {
+    Result(
+      header = ResponseHeader(400, Map(CONTENT_TYPE -> "text/plain")),
+      body = Enumerator("Bad Request!".getBytes())
+    )
+  }
+
+  def error = Action {
+    Result(
+      header = ResponseHeader(500, Map(CONTENT_TYPE -> "text/plain")),
+      body = Enumerator("Internal Server Error!".getBytes())
+    )
+  }
 }
