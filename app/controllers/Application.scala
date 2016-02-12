9574ad03f7befbabcb6443447442154900c248cb
shigemk2 <i.am.shige@gmail.com>
2016-02-13 01:04:18 +0900

    POST

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,6 +3,10 @@ package controllers
 import play.api._
 import play.api.mvc._
 import play.api.libs.iteratee.Enumerator
+import play.api.data._
+import play.api.data.Forms._
+
+case class User(name: String, email: String)
 
 class Application extends Controller {
 
@@ -28,6 +32,32 @@ class Application extends Controller {
     Result(
       header = ResponseHeader(500, Map(CONTENT_TYPE -> "text/plain")),
       body = Enumerator("Internal Server Error!".getBytes())
+    )
+  }
+
+  val userDataForm: Form[User] = Form (
+    mapping(
+      "name" -> text,
+      "email"-> text
+    )(User.apply)(User.unapply)
+  )
+
+  def create = Action { implicit request =>
+    userDataForm.bindFromRequest.fold(
+      errors => {
+        Result(
+          header = ResponseHeader(400, Map(CONTENT_TYPE -> "text/plain")),
+          body = Enumerator("Bad Request!".getBytes())
+        )
+      },
+      userdata => {
+        println(userdata.name)
+        println(userdata.email)
+        Result(
+          header = ResponseHeader(200, Map(CONTENT_TYPE -> "text/plain")),
+          body = Enumerator("OK!".getBytes())
+        )
+      }
     )
   }
 }
